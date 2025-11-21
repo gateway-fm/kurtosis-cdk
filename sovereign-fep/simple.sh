8246de99c842523b1729f72b2be75f7c9340d129
Scott Fairclough <scott@hexosoft.co.uk>
2025-11-21 13:28:25 +0000

    use new fep hardfork and updated aggchain proof vkey

--- sovereign-fep/simple.sh
+++ sovereign-fep/simple.sh
@@ -16,7 +16,7 @@ pwd=$(pwd)
 ./stop.sh
 
 vkeySelector="0x00070001" # hard coded to match the vkey selector from the agg prover program in aggkit/provers repo
-proversRepoVKey="0x70d061b24b1d8e5e73705be213bbcd1d20e154a74483849c3decaf0808d471b6"
+proversRepoVKey="0x2ea95de1270cce23390aa9da08bc9d5d6be7afeb421b173b6eefb469512bdf60"
 aggregationVkey="0x00afb45d8064ae10aa6a1793b8f39a24c27268efae2917b5c02950b2377fbf00"
 rangeVkeyCommitment="0x416d710344b6b6fa2a0b1a1445f3d6ba4fdd5ab43f0e863b1c522db20f28ad9b"
 
